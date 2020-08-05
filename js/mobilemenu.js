3073158e55181aba1e0458954e860661cdcfb99c
Bartłomiej Profic <45732082+flaszuu@users.noreply.github.com>
2020-08-05 15:32:24 +0200

    Initial commit

---
+++ js/mobilemenu.js
@@ -0,0 +1,86 @@
+/*let mobileToggle = document.getElementById("mobile-toggle");
+let mobileList = document.getElementById("main-menu");
+let mobileHeader = document.getElementById("header");
+
+
+
+mobileToggle.addEventListener('click', openMenu);
+
+function openMenu() {
+    if (mobileToggle.classList.contains("active")){
+        mobileToggle.classList.remove("active");
+        mobileList.classList.remove("active");
+        mobileHeader.classList.remove("active");
+    } else {
+        mobileToggle.classList.add("active");
+        mobileList.classList.add("active");
+        mobileHeader.classList.add("active");
+    }
+}*/
+
+
+data = true;
+
+
+$('.feature').hover(function () {
+    data = false;
+    var link = $(this)[0].getBoundingClientRect();
+
+    var x_pos = (link.left);
+    var y_pos = (link.top);
+
+    var translate = 'translate(' + x_pos + 'px,' + y_pos + 'px)';
+
+    $('.underlay').css({
+        'transform': translate,
+        'width': link.width + 'px',
+        'height': link.height * 0.95 + 'px',
+        'opacity': 1,
+        'border-radius': '0'
+    });
+}, function () {
+    data = true;
+});
+
+$('.footer-activator').hover(function () {
+    data = false;
+    var link = $(this)[0].getBoundingClientRect();
+
+    var x_pos = (link.left);
+    var y_pos = (link.top);
+
+    var translate = 'translate(' + x_pos + 'px,' + y_pos + 'px)';
+
+    $('.underlay').css({
+        'transform': translate,
+        'width': link.width + 'px',
+        'height': link.height + 'px',
+        'opacity': 1,
+        'border-radius': '50%',
+
+    });
+}, function () {
+    data = true;
+});
+
+
+$(document).on('mousemove', function (e) {
+    $('body').css({
+        'cursor' : 'default'
+    });
+
+    if(data) {
+        var xPos = e.pageX - 5;
+        var yPos = e.pageY - 5;
+        // console.log(xPos, yPos);
+        var translate = 'translate(' + xPos + 'px,' + yPos + 'px)';
+        $('.underlay').css({
+            'transform': translate,
+            'height': '10px',
+            'width': '10px',
+            'border-radius': '50%',
+            'opacity': 1
+        });
+    }
+
+});
